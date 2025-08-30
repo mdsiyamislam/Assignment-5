652cff6c69b25998f8bc59a200703118aba1e740
Your Name <you@example.com>
2025-08-31 00:12:35 +0600

    added heart btn fanction

--- home.js
+++ home.js
@@ -1,1 +1,19 @@
 
+let heartCount = 0;
+    const heartCountEl = document.getElementById("heartCount");
+    const hearts = document.querySelectorAll(".heart");
+
+    hearts.forEach(heart => {
+      heart.addEventListener("click", function () {
+        // যদি আগে লাইক না করা থাকে
+        if (!heart.classList.contains("liked")) {
+          heart.classList.add("liked");
+          heartCount++;
+        } else {
+          // আবার ক্লিক করলে count কমবে
+          heart.classList.remove("liked");
+          heartCount--;
+        }
+        heartCountEl.textContent = heartCount;
+      });
+    });
